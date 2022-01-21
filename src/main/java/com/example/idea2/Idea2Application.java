3e58d8934d7a64a9c0e195f439f4c00c8cdf7dc1
morning020917 <89618961+morning020917@users.noreply.github.com>
2022-01-21 22:46:32 +0800

    Initial commit

---
+++ src/main/java/com/example/idea2/Idea2Application.java
@@ -0,0 +1,12 @@
+package com.example.idea2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Idea2Application {
+
+    public static void main(String[] args) {SpringApplication.run(Idea2Application.class, args);
+    }
+
+}
